cf717c3c958c6706490de9816fa2ec66aaf28df5
OtsukaHiromichi <hiro_o2o2@hiro-no-MacBook-Air.local>
2015-06-14 23:43:58 +0900

    Initial Commit

---
+++ Jinro-Wa-Tests/Jinro_Wa_Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Jinro_Wa_Tests.swift
+//  Jinro-Wa-Tests
+//
+//  Created by OtsukaHiromichi on 2015/06/14.
+//  Copyright (c) 2015年 OtsukaHiromichi. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Jinro_Wa_Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
